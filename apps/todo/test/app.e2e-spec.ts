034b4844fe082c5fef27572466839a63ca821dc8
Stas Polos <polos_ss@groupbwt.com>
2023-03-06 13:58:01 +0200

    feat: add e2e tests for task.controller.ts

--- apps/todo/test/app.e2e-spec.ts
+++ apps/todo/test/app.e2e-spec.ts
@@ -1,24 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { TodoModule } from '../src/todo.module';
+import { TasksController } from '../src/tasks/tasks.controller';
+import { TasksService } from '../src/tasks/tasks.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Task } from '../src/tasks/task.entity';
+import { LOGS_CONSUMER } from '../src/todo.constants';
+import { clientProxyFactoryMock } from '../src/tasks/mock';
 
-describe('AppController (e2e)', () => {
+jest.mock('rxjs', () => {
+  const original = jest.requireActual('rxjs');
+
+  return {
+    ...original,
+    lastValueFrom: () =>
+      new Promise((resolve, reject) => {
+        resolve(true);
+      }),
+  };
+});
+
+describe('TaskController (e2e)', () => {
   let app: INestApplication;
+  let find: jest.Mock;
+  let insert: jest.Mock;
 
   beforeEach(async () => {
+    find = jest.fn();
+    insert = jest.fn();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [TodoModule],
+      controllers: [TasksController],
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(Task),
+          useValue: [find, insert],
+        },
+        {
+          provide: LOGS_CONSUMER,
+          useFactory: clientProxyFactoryMock,
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('/api');
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/api/tasks (POST)', () => {
+    describe('if input values is correct', () => {
+      it('should create new task and return status 201', async () => {
+        const validDto = { title: 'title', description: 'description' };
+        return request(app.getHttpServer())
+          .post('/api/tasks')
+          .send(validDto)
+          .expect(HttpStatus.CREATED);
+      });
+    });
+    describe('if input values is not correct', () => {
+      it('should throw validation error', async () => {
+        const noValidDto = { title: 1, description: 2 };
+        return request(app.getHttpServer())
+          .post('/api/tasks')
+          .send(noValidDto)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
